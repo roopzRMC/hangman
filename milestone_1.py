78dac53816cd8513e41965b4753a68a23f9467aa
Rupert Coghlan <r.coghlan@gmail.com>
2022-10-16 11:39:31 +0100

    adding random.choice for fruit selection

--- milestone_1.py
+++ milestone_1.py
@@ -1,6 +1,13 @@
+# %%
+import random
+
 ## create a list of 5 favourite fruits
 # %%
 word_list = ['Mango', 'Plum', 'Nectarine', 'Orange', "Banana"]
 # %%
 print(word_list)
 # %%
+## use random choice to select at random a fruit from the list
+word = random.choice(word_list)
+print(word)
+
