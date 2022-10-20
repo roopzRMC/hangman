f0790e84173933f80d0f620db2a73705fda4fac5
Rupert Coghlan <r.coghlan@gmail.com>
2022-10-20 17:15:47 +0100

    adding range_checker to defining functions

--- defining_functions.py
+++ defining_functions.py
@@ -5,3 +5,11 @@ def my_void_function():
 # %%
 my_void_function()
 # %%
+def range_checker(lower_bound, upper_bound, number):
+    if number > lower_bound and number < upper_bound:
+        print(f'number is between {lower_bound} and {upper_bound}')
+    else:
+        print(f'number is NOT between {lower_bound} and {upper_bound}')
+# %%
+range_checker(2, 10, 6)
+# %%
